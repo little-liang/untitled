e624035840c4bcb67e5c1d29d7d5b5afd24e5487
Administrator <740658054@qq.com>
2017-04-13 16:23:56 +0800

    ceshi23kkk

--- online/search_oracle_table_for_audo_java_db/update_ctl_fc_table.py
+++ online/search_oracle_table_for_audo_java_db/update_ctl_fc_table.py
@@ -5,10 +5,25 @@ import sys
 db_223 = "etl/etl@10.138.22.223:1521/edw"
 db_226_etl = "etl/etl_Haier@10.138.22.226:1521/edw"
 
-conn = cx_Oracle.connect(db_226_etl)
+conn = cx_Oracle.connect(db_223)
 cur = conn.cursor()
 
-result = cur.execute("update ctl_fc set data_date='20170403' where system_id in ('fchry','fccf')")
+job_info = {}
+
+now_date_time = '20170101'
+task_group_id = 1
+job_id = 1
+job_info['storeprodure_name'] = 'ggg'
+start_time = 'dddd'
+end_time = 'fff'
+run_status = 'fff'
+job_info['run_time'] = 'fff'
+
+
+
+sql = "insert into etl.t_jobs_logs(DATA_DAY,GROUP_ID,JOB_ID,PRO_NAME, RUN_START_TIME,RUN_END_TIME,RUN_STATUS, RUN_TIME)values(to_date(20170101,'yyyymmdd'),1,1,'ffff',to_date('20161102 10:03','yyyymmdd hh24:mi'),to_date('20161102 10:03','yyyymmdd hh24:mi'),'D','09:00')"
+
+result = cur.execute(sql)
 conn.commit()
 cur.close()
 conn.close()
--- online/test/linshi4.py
+++ online/test/linshi4.py
@@ -1,17 +1,6 @@
-import os
-# os.remove('status.txt')
 
 
 
-
-
-
-
-
-
-
-f2 = open('status2.txt', 'a')
-
-f2.writelines(('20170101 ', '06:00 ', 'R', '\n'))
-
-f2.close()
+a = {'1'}
+if '1' in a:
+       print("g")
